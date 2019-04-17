bfac00aa2ae434ccbb5a0126f1545483add8f9b9
JoannaArechiga <issassam95@hotmail.com>
2019-04-17 12:39:35 -0500

    Funciones organizadas con tareas específicas, para generar la data.

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,37 @@
+
+
 let visitorName = document.querySelector("#visitor");
 let visitingCo = document.querySelector("#selectCoworker");
-
 const btnRegister = document.querySelector("#register");
+btnRegister.addEventListener('click', submitData);
+
+function submitData (){
+  validateInputs ();
+};
 
 
+// Esta funcion crea el documento con los datos del visitante en Firebase
 
-function registerVisitant () {
-  if (visitorName.value.length < 5 || visitingCo.value === "" ) {
-    alert ("Datos incorrectos" );
-  
-  } else {
+function createCollection () {
     db.collection("visitors").add({
-      name: visitorName.value,
-      visiting: visitingCo.value,
-      hour: firebase.firestore.FieldValue.serverTimestamp(),
-      photo: null //y aquí la de la fotografía
+        name: visitorName.value,
+        visiting: visitingCo.value,
+        hour: firebase.firestore.FieldValue.serverTimestamp(), //new Date
+        photo: null //Fotografía pendiente en base 64
     });
-    alert("Gracias por visitarnos");
-    visitorName.value = "";
-    visitingCo.value = "";
-  };
+      alert("Gracias por tú visita");
+      visitorName.value = "";
+      visitingCo.value = "";
 }
+  
+// Función que valida que los campos no estén vacíos 
+function validateInputs () {
+if (visitorName.value.length < 5 || visitingCo.value === "" ) {
+ alert ("Tu información no está completa");
 
-if (btnRegister) {
-  btnRegister.addEventListener("click", registerVisitant);
+} else {
+  createCollection();
+  };
 }
 
 const video = document.getElementById('video');
@@ -56,81 +64,3 @@ const constraints = {
             
             // Load init
             init();
-
-//probando lo de la cámara
-/*const captureVideoButton = document.querySelector('#screenshot .capture-button');
-const screenshotButton = document.querySelector('#screenshot-button');
-const img = document.querySelector('#screenshot img');
-const video = document.querySelector('#screenshot video');
-
-
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-  navigator.mediaDevices.getUserMedia);
-}
-
-if (hasGetUserMedia()) {
-  // Good to go!
-} else {
-  alert('getUserMedia() is not supported by your browser');
-}
-
-const constraints = {
-  video: true
-};
-
-const videoCamera = document.querySelector('video');
-
-navigator.mediaDevices.getUserMedia(constraints).
-then((stream) => {video.srcObject = stream});
-
-
-
-const canvas = document.getElementById('canvas');
-
-captureVideoButton.onclick = function() {
-  navigator.mediaDevices.getUserMedia(constraints).
-  then(handleSuccess).catch(handleError);
-};
-
-screenshotButton.onclick = video.onclick = function() {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  canvas.getContext('2d').drawImage(video, 0, 0);
-  // Other browsers will fall back to image/png
-  img.src = canvas.toDataURL('image/webp');
-};
-
-function handleSuccess(stream) {
-  screenshotButton.disabled = false;
-  video.srcObject = stream;
-}
-
-
-/*window.addEventListener("load",init);
-function init(){
-  let video = document.querySelector("#v");
-  let canvas = document.querySelector("#c");
-  let btn = document.querySelector("#t");
-  let img = document.querySelector("#img");
-  
-  navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-  if(navigator.getUserMedia){
-    navigator.getUserMedia({video:true},function(stream){
-      video.src= window.URL.createObjectURL(stream);
-      video.play();
-    },function(e){console.log(e);})
-  }
-  else alert("error de navegador");
-    video.addEventListener("loadedmetadata", 
-    function(){
-      canvas.width = video.videoWidth; 
-      canvas.height = video.Height;
-    },false);
-  
-  btn.addEventListener('click', function(){
-    canvas.getContext("2d").drawImage(video,0,0);
-    let imgData = canvas.toDataURL("image/png");
-    img.setAttribute("src",imgData);
-  });
- */
